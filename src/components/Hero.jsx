f7153534191d8c108efc4d605651519acc30b3ad
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 00:01:58 -0300

    *3;

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,17 @@
-import '../styles/hero.css'
-import '../styles/globals.css'
+import '../styles/hero.css';
+import '../styles/globals.css';
 
 export default function Hero() {
     return (
         <>
-            
+         <section className='hero'>
+            <div className='container hero-content'>
+                <h1>Iphone 16</h1>
+                <div className='hero-tagline'>Built for Apple Intelligence.</div>
+                <p></p>
+            </div>
+
+         </section>
         </>
     )
 }
